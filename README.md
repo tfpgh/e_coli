be40d28c037236e7c2c98cf6dd66cdaf581975c8
Toby Penner <tobypenner01@gmail.com>
2026-05-14 18:37:56 -0400

    Update posters

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Mapping the Protein Pangenome of 20 E. coli Strains. Middlebury College CSCI 0321 Final Project.
 
-![Project Poster](poster.png)
+![Project Poster v2](poster-v2.png)
+
+![Project Poster v1](poster-v1.png)
--- poster/export_gephi.py
+++ poster/export_gephi.py
@@ -25,14 +25,41 @@ for cluster_id, members in clusters.items():
     for protein_idx in members:
         protein_to_cluster[protein_idx] = cluster_id
 
+n_genomes = len({p.genome_id for p in dataset.proteins})
+
+CATEGORY_SINGLE_COPY_CORE = 0
+CATEGORY_OTHER_CORE = 1
+CATEGORY_ACCESSORY = 2
+CATEGORY_STRAIN_UNIQUE = 3
+CATEGORY_UNCLUSTERED = 4
+
+cluster_to_category: dict[int, int] = {}
+for cluster_id, members in clusters.items():
+    genome_counts: dict[str, int] = {}
+    for protein_idx in members:
+        gid = dataset.proteins[protein_idx].genome_id
+        genome_counts[gid] = genome_counts.get(gid, 0) + 1
+    n_genomes_in_cluster = len(genome_counts)
+    if n_genomes_in_cluster == 1:
+        cluster_to_category[cluster_id] = CATEGORY_STRAIN_UNIQUE
+    elif n_genomes_in_cluster < n_genomes:
+        cluster_to_category[cluster_id] = CATEGORY_ACCESSORY
+    elif len(members) == n_genomes and all(c == 1 for c in genome_counts.values()):
+        cluster_to_category[cluster_id] = CATEGORY_SINGLE_COPY_CORE
+    else:
+        cluster_to_category[cluster_id] = CATEGORY_OTHER_CORE
+
 
 with open("data/gephi_nodes.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["Id", "Genome", "Length", "Cluster", "Color"])
+    writer.writerow(["Id", "Genome", "Length", "Cluster", "Color", "CategoryColor"])
     for i, p in enumerate(dataset.proteins):
         cluster_id = protein_to_cluster.get(i, -1)
         color = cluster_id % N_COLORS if cluster_id >= 0 else N_COLORS
-        writer.writerow([i, p.genome_id, len(p.protein_str), cluster_id, color])
+        category_color = cluster_to_category.get(cluster_id, CATEGORY_UNCLUSTERED)
+        writer.writerow(
+            [i, p.genome_id, len(p.protein_str), cluster_id, color, category_color]
+        )
 
 n_edges = 0
 with open("data/gephi_edges.csv", "w", newline="") as f:
